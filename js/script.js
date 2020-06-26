27c7bf82139e8e9c63bf36849bf9e61b6644bf63
Tyler Degand <tylerdegand@gmail.com>
2020-06-26 16:18:06 -0500

    pagination links there (needs work and debugging)

--- js/script.js
+++ js/script.js
@@ -5,17 +5,6 @@ FSJS project 2 - List Filter and Pagination
 
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const studentList = document.getElementsByTagName('li');
 const studentsPerPage = 10;
 
@@ -53,17 +42,32 @@ const showPage = (list, page) => {
 ***/
 
 const appendPageLinks = (list) => {
+   const maxPages = Math.floor(list.length / studentsPerPage);
    const parentDiv = document.querySelector('.page');
    const pagDiv = document.createElement('div');
+   parentDiv.appendChild(pagDiv).className = 'pagination';
    const newList = document.createElement('ul');
-   const newListItem = document.createElement('li');
-   const maxPages = list.length / studentsPerPage;
+   pagDiv.appendChild(newList);
 
+   for (let i = 0; i < maxPages; i++) {
+      const newListItem = document.createElement('li');
+      newList.appendChild(newListItem);
+      newListItem.innerHTML = `<a href="#">${i + 1}</a>`;
+   }
+   const paginationLinks = document.getElementsByTagName('a');
 
+   pagDiv.addEventListener('click', (event) => {
+      for (let j = 0; j < paginationLinks.length; j++) {
+         if (event.target.classList.contains('active') === false) {
+            event.target.classList.add('active');
+         } else if (newList.children.classList === 'active') {
+            event.target.classList.remove('active');
+         }
+      }
+      event.preventDefault();
+   });
 
 }
-
 appendPageLinks(studentList);
-
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
